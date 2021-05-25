3f3ff6280e8336ebde3b4b2c94ab19f2b95b4d6a
paddy blake <paddyblake09@hotmail.com>
2021-05-25 10:10:35 +0100

    Updates

---
+++ precache-manifest.3226ad740d74b026c0bbf885ea733773.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8f7db4dafcb2db59f74459f0eb3892a",
+    "url": "https://BuildingOnCardano.github.io/buildingoncardano/index.html"
+  },
+  {
+    "revision": "136399570a3b8bb5351c",
+    "url": "https://BuildingOnCardano.github.io/buildingoncardano/static/css/main.b06e38fa.chunk.css"
+  },
+  {
+    "revision": "613046ca0fd5265b76cd",
+    "url": "https://BuildingOnCardano.github.io/buildingoncardano/static/js/2.3081d2df.chunk.js"
+  },
+  {
+    "revision": "136399570a3b8bb5351c",
+    "url": "https://BuildingOnCardano.github.io/buildingoncardano/static/js/main.a6c316a6.chunk.js"
+  },
+  {
+    "revision": "423a8b95cf3b180a7888",
+    "url": "https://BuildingOnCardano.github.io/buildingoncardano/static/js/runtime~main.3200014f.js"
+  },
+  {
+    "revision": "978e51b554db00a67e2796661b3526fe",
+    "url": "https://BuildingOnCardano.github.io/buildingoncardano/static/media/100_4.978e51b5.jpg"
+  },
+  {
+    "revision": "b70185405512b7dc2b6da99db0b1897d",
+    "url": "https://BuildingOnCardano.github.io/buildingoncardano/static/media/logo_200.b7018540.png"
+  },
+  {
+    "revision": "f7f5fc61b5a82360ce50a2af91ae66ab",
+    "url": "https://BuildingOnCardano.github.io/buildingoncardano/static/media/rsz_1sidebar-4.f7f5fc61.jpg"
+  }
+]);
