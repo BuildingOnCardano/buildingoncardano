b333d5538dfd0250d167c2ec47b385d47b77dc12
paddy blake <paddyblake09@hotmail.com>
2021-05-26 16:15:22 +0100

    styling updates and footer

--- src/components/Layout/Footer.js
+++ src/components/Layout/Footer.js
@@ -1,13 +1,36 @@
 import React from 'react';
+import shamrock from 'assets/img/shamrock.png';
+import paul from 'assets/img/paul.jpg';
 
-import { Navbar, Nav, NavItem } from 'reactstrap';
+import { Navbar, Nav, NavItem, Row } from 'reactstrap';
 const Footer = () => {
   return (
     <Navbar>
       <Nav navbar>
-        {/* <NavItem>
-          Building On Cardano - brought to you by Shamrock Pool and Cardano With Paul
-        </NavItem> */}
+        <NavItem>
+          <Row>
+            Building On Cardano - a place to view whats happening within the cardano ecosystem. 
+          </Row>
+          <Row>
+            Brought to you by SPO's Shamrock Pool and Cardano With Paul
+          </Row>
+          <Row>
+            <a href="https://shamrock-pool.com/" target="_blank">
+              <img
+                src={shamrock}
+                className="rounded"
+                style={{ width: 100, height: 100, cursor: 'pointer' }}
+              />
+            </a>
+            <a href="https://www.cardanowithpaul.com/" target="_blank">
+              <img
+                src={paul}
+                className="rounded"
+                style={{ width: 100, height: 100, cursor: 'pointer' }}
+              />
+            </a>
+          </Row>
+        </NavItem>
       </Nav>
     </Navbar>
   );
