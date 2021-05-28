07388e76174b02845825125a87556ccb9ea3a72b
paddy blake <paddyblake09@hotmail.com>
2021-05-28 22:57:36 +0100

    add verified

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -45,16 +45,17 @@ export default class ProjectCard extends React.Component {
             }}>
                 <br></br>
                 {this.props.myprojectspage && (<Link to={{ pathname: '/editproject/'+this.props.projectDetails.name}}>Edit Project</Link>)}
-
+                {this.props.myprojectspage && (<p>Verified: {this.props.projectDetails.verified}</p>)}
                 <Link to={{ pathname: '/projectdetails/'+this.props.projectDetails.name, state: { projectDetails: this.props.projectDetails } }}>
-                    <div style={{ paddingTop: 15, alignSelf: 'flex-start' }}>
+                    <div style={{ paddingTop: 10, alignSelf: 'flex-start' }}>
                         <ReactImageFallback
                             src={this.props.img}
-                            width="40"
+                            width="50"
                             height="70"
                             fallbackImage={CardanoImage} />
 
                         <div className="ProjectCard-body">
+
                             <h2>{this.props.projectDetails.name}</h2>
                             <h4>{this.props.projectDetails.description}</h4>
                             <h5>{this.props.projectDetails.type}</h5>
