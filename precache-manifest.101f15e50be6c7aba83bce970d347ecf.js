11843399d1067fc596747ad2b8db9845fe296461
paddy blake <paddyblake09@hotmail.com>
2021-06-04 22:00:45 +0100

    Updates

---
+++ precache-manifest.101f15e50be6c7aba83bce970d347ecf.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5483c45cbf452972df6647f81c973f52",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8b1d1064942a03f39a40",
+    "url": "/static/css/2.b9f073f1.chunk.css"
+  },
+  {
+    "revision": "34663fcf156cfa331955",
+    "url": "/static/css/main.9914744f.chunk.css"
+  },
+  {
+    "revision": "8b1d1064942a03f39a40",
+    "url": "/static/js/2.bcf5260a.chunk.js"
+  },
+  {
+    "revision": "34663fcf156cfa331955",
+    "url": "/static/js/main.ff71103b.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "978e51b554db00a67e2796661b3526fe",
+    "url": "/static/media/100_4.978e51b5.jpg"
+  },
+  {
+    "revision": "695fd372940559eafc67708a061a8bb6",
+    "url": "/static/media/Light-icon200.695fd372.png"
+  },
+  {
+    "revision": "b70185405512b7dc2b6da99db0b1897d",
+    "url": "/static/media/logo_200.b7018540.png"
+  },
+  {
+    "revision": "450f0f816916353a7d29911d2ca21491",
+    "url": "/static/media/paul.450f0f81.jpg"
+  },
+  {
+    "revision": "f7f5fc61b5a82360ce50a2af91ae66ab",
+    "url": "/static/media/rsz_1sidebar-4.f7f5fc61.jpg"
+  },
+  {
+    "revision": "f98b92d52709d5a212ebeec3fd5f7a6d",
+    "url": "/static/media/shamrock.f98b92d5.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.1cf5054c290bc3f0bc7b9b7f6283bacc.js"
+  "/precache-manifest.101f15e50be6c7aba83bce970d347ecf.js"
 );
 
 self.addEventListener('message', (event) => {
